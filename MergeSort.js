cfcb0e02c34e556d1ed703c19f04b19832d5d7bc
Emre Yılmaz <83725010+emreylmz815@users.noreply.github.com>
2022-04-19 22:48:08 +0300

    Update MergeSort.js

--- MergeSort.js
+++ MergeSort.js
@@ -22,4 +22,4 @@ function mergeSort(array) {
       return [...merged, ...array1, ...array2];
     }
   }
-  console.log(mergeSort([1,4,2,8,345,123,43,32,5643,63,123,43,2,55,1,234,92]))
+  mergeSort([1,4,2,8,345,123,43,32,5643,63,123,43,2,55,1,234,92])
