a6c989c49c81036917aad1cd75695e49b845d0b7
Emre Yılmaz <83725010+emreylmz815@users.noreply.github.com>
2022-04-19 22:47:47 +0300

    Add files via upload

---
+++ MergeSort.js
@@ -0,0 +1,25 @@
+function mergeSort(array) {
+    if (array.length === 1) {
+      return array;
+    } else {
+      const splitIndex = Math.floor(array.length / 2);
+      return merge(
+        mergeSort(array.slice(0, splitIndex)),
+        mergeSort(array.slice(splitIndex))
+      );
+    }
+    function merge(array1, array2) {
+      let merged = [];
+      while (array1.length && array2.length) {
+        if (array1[0] < array2[0]) {
+          merged.push(array1.shift());
+        } else if (array1[0] > array2[0]) {
+          merged.push(array2.shift());
+        } else {
+          merged.push(array1.shift(), array2.shift());
+        }
+      }
+      return [...merged, ...array1, ...array2];
+    }
+  }
+  console.log(mergeSort([1,4,2,8,345,123,43,32,5643,63,123,43,2,55,1,234,92]))
